d076924435f139339d739e748bfd4c8100c3ad7d
David Cândido de Souza <dcs2@cesar.school>
2022-09-29 22:36:09 -0300

    Pequenas mudanças e correção do template

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+# path("", views.index, name = "index"),
+# path("v1/", views.v1, name = "view 1"),
+# ]
+
 urlpatterns = [
-path("", views.index, name = "index"),
-path("v1/", views.v1, name = "view 1"),
+    path("<str:name>", views.index, name = "index"),
+    path("", views.home, name="home")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
+from xml.dom.minidom import Identified
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Playlist, Items
+
 # Create your views here.
-def index(response):
-	return HttpResponse("nautico")
-def v1(response):
-	return HttpResponse("nautocpppp")
+def index(response, name):
+	ls = Playlist.objects.get(name=name)
+	return render(response, "main/base.html", {})
+
+def home(response):
+	return render(response, "main/home.html", {})
