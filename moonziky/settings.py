f1cd13b5198625462de544f86796e3b3f3756bcb
FernandoLGM <112346915+FernandoLGM@users.noreply.github.com>
2022-10-24 19:45:06 -0300

    Add files via upload

--- moonziky/settings.py
+++ moonziky/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+cb9%4g^05y^0x)rk%!bk^rq9s^c_l=@7os-av0r+*h3_rrq$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
