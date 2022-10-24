e45bcb6ddfb73036212d1f8ca96d7bd531eed562
FernandoLGM <112346915+FernandoLGM@users.noreply.github.com>
2022-10-24 20:14:40 -0300

    Add files via upload

--- main/models.py
+++ main/models.py
@@ -3,15 +3,6 @@ from django.urls import reverse
 
 
 # Create your models here.
-class Playlist(models.Model):
-    name=models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('playlist-detail', args=[str(self.id)])
-    
 class Music(models.Model):
     playlist=models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='musics')
     name=models.CharField(max_length=300)
@@ -19,3 +10,14 @@ class Music(models.Model):
 
     def __str__(self):
         return self.name
+
+class Playlist(models.Model):
+    name=models.CharField(max_length=200)
+    song=models.ManyToManyField(Music, on_delete=models.CASCADE, blank=True, default=None, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('playlist-detail', args=[str(self.id)])
+    
--- main/views.py
+++ main/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Playlist, Music
 from django.views import generic
+from django.views.decorators.http import require_POST
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def playlistPage(response, playlistName):
@@ -14,6 +19,10 @@ def home(response):
 
 def index(request):
     """View function for home page of site."""
+	if request.user.is_authenticated:
+		return HttpResponseRedirect("/home/")
+	else:
+		return render(request, "index.html")
     
     numPlaylist = Playlist.objects.all().count()
     numMusic = Music.objects.count()
@@ -40,4 +49,60 @@ class PlaylistListView(generic.ListView):
 
 class PlaylistDetailView(generic.DetailView):
 	model = Playlist
+
+def createplaylist_view(request):
+	songs=song.objects
+	if request.method == 'POST':
+		playlist=Playlist()
+		playlist=name=request.POST['nomedaplaylist']
+		playlist.save()
+		print(playlist.name)
+		playlist.songs.add(*songs)
+		return render(request, 'addsongs.html', {'songs':songs})
+	else:
+		return render(request, 'createplaylist.html',{'songs':songs})
+
+def addsongs_view(request,pk):
+	songs=Song.objects.all()
+	playlist=Playlist.objects
+	if request.method == 'POST':
+		item=Song.objects.get(id=pk)
+		playlist.song=item
+		playlist.objects.update(song=item)
+		return render(request, 'addsongs.html', {'songs':songs})
+		return render(request, 'addsongs.html', {'songs':songs})
+
+@require_POST
+def cadastrar_usuario(request):
+	try:
+		usuario_aux = User.objects.get(email=request.POST['campo-email'])
+
+		if usuario_aux:
+			return render(request, 'index.html', {'msg': 'Erro! Ja existe um usuario com o mesmo email'})
+
+	except User.DoesNotExist:
+		Cadastrar usuário
+
+	nome_usuario = request.POST['uname']
+	email = request.POST['email']
+	senha = request.POST['password']
+
+	newUser = User.objects.create_user(username=nome_usuario, email=email, password=senha)
+	newUser.save()
+
+@require_POST
+def entrar(request):
+	usuario_aux = User.objects.get(email=require.POST['email'])
+	usuario = authenticate(username=usuario_aux.username,
+							password=request.POST["senha"])
+	if usuario is not None:
+		login(request, usuario)
+		return HttpResponseRedirect('/home/')
+
+	return HttpResponseRedirect('/')
+
+@login_required
+def sair(request):
+	logout(request)
+	return HttpResponseRedirect('/')
 
