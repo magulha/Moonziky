e58008d24812700f37687a107f7abd38b875df27
FernandoLGM <112346915+FernandoLGM@users.noreply.github.com>
2022-10-24 20:09:35 -0300

    Add files via upload

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     path('playlist/', views.PlaylistListView.as_view(), name='playlist'),
     path('playlist/<int:pk>', views.PlaylistDetailView.as_view(), name='playlist-detail'), # 1º A URL, 2º A View, 3º O nome da View
     #path('musics/<int:pk>', views.MusicDetailView.as_view(), name='musics-detail'),
-    path('createplaylist/',views.createplaylist_view,name='createplaylist'),
-    path('createplaylist/<int:pk>',views.addsongs_view,name='addsongs')
 ]
